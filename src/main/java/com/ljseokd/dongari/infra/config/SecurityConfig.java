3d4f573cafd35db1daf97fc8f0e1ff178afd441d
JoongSeokD <ljseokd@gmail.com>
2020-07-20 17:26:36 +0900

    시큐리티 기본 환경 설정 셋팅

---
+++ src/main/java/com/ljseokd/dongari/infra/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.ljseokd.dongari.infra.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login");
+
+        http.logout()
+                .logoutSuccessUrl("/");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
