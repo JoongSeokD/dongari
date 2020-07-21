edda58938a65becd88ef48641217f9e67056391c
JoongSeokD <ljseokd@gmail.com>
2020-07-21 19:50:25 +0900

    회원 가입 폼 데이터

---
+++ src/main/java/com/ljseokd/dongari/modules/account/SignUpForm.java
@@ -0,0 +1,27 @@
+package com.ljseokd.dongari.modules.account;
+
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@Builder
+public class SignUpForm {
+
+    @NotBlank
+    @Length(min = 3, max = 20)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9_]{3,20}$")
+    private String nickname;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Length(min = 8, max = 50)
+    private String password;
+}
