ab22f456632fde5a08cef8d6b1466e5a86969ee4
JoongSeokD <ljseokd@gmail.com>
2020-07-21 19:50:07 +0900

    회원 가입과 Spring Security 인증 문제 해결

---
+++ src/main/java/com/ljseokd/dongari/modules/account/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.ljseokd.dongari.modules.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByEmail(String emailOrNickname);
+    Account findByNickname(String emailOrNickname);
+}
---
+++ src/main/java/com/ljseokd/dongari/modules/account/AccountService.java
@@ -0,0 +1,45 @@
+package com.ljseokd.dongari.modules.account;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccountService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+
+
+    public Account processNewAccount(SignUpForm signUpForm){
+        return saveNewAccount(signUpForm);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        signUpForm.setPassword(passwordEncoder.encode(signUpForm.getPassword()));
+        Account account = modelMapper.map(signUpForm, Account.class);
+        return accountRepository.save(account);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String emailOrNickname) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(emailOrNickname);
+        if (account == null){
+            account = accountRepository.findByNickname(emailOrNickname);
+        }
+        if (account == null){
+            throw new UsernameNotFoundException(emailOrNickname);
+        }
+
+        return new UserAccount(account);
+    }
+}
