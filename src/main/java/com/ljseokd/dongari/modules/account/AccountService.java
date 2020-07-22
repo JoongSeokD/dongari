3285d0cf79b1094e3c75800f661ddd04c77fd3a2
JoongSeokD <ljseokd@gmail.com>
2020-07-22 10:22:13 +0900

    회원 가입 후 인증 토큰 메일 발송 (콘솔 테스트)

--- src/main/java/com/ljseokd/dongari/modules/account/AccountService.java
+++ src/main/java/com/ljseokd/dongari/modules/account/AccountService.java
@@ -1,5 +1,7 @@
 package com.ljseokd.dongari.modules.account;
 
+import com.ljseokd.dongari.infra.mail.EmailMessage;
+import com.ljseokd.dongari.infra.mail.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -22,11 +25,20 @@ public class AccountService implements UserDetailsService {
     private final AccountRepository accountRepository;
     private final PasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
+    private final EmailService emailService;
 
 
     public void processNewAccount(SignUpForm signUpForm){
         Account account = saveNewAccount(signUpForm);
         login(account);
+        sendSignUpConfirmEmail(account);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        signUpForm.setPassword(passwordEncoder.encode(signUpForm.getPassword()));
+        Account account = modelMapper.map(signUpForm, Account.class);
+        account.generateEmailCheckToken();
+        return accountRepository.save(account);
     }
 
     private void login(Account account) {
@@ -37,11 +49,16 @@ public class AccountService implements UserDetailsService {
         SecurityContextHolder.getContext().setAuthentication(token);
     }
 
-    private Account saveNewAccount(SignUpForm signUpForm) {
-        signUpForm.setPassword(passwordEncoder.encode(signUpForm.getPassword()));
-        Account account = modelMapper.map(signUpForm, Account.class);
-        return accountRepository.save(account);
+    private void sendSignUpConfirmEmail(Account account) {
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(account.getEmail())
+                .subject("dongari, 회원 가입 인증")
+                .message("회원 가입 인증 토큰 : " + account.getEmailCheckToken())
+                .build();
+        emailService.sendEmail(emailMessage);
+        account.emailCheckTokenGeneratedAt();
     }
+
 
     @Transactional(readOnly = true)
     @Override
