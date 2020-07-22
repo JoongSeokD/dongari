d6e3107bccc88c6409c09199e3817afd0f1f07bf
JoongSeokD <ljseokd@gmail.com>
2020-07-22 10:02:08 +0900

    회원 가입 처리

---
+++ src/main/java/com/ljseokd/dongari/modules/account/AccountController.java
@@ -0,0 +1,43 @@
+package com.ljseokd.dongari.modules.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model){
+        model.addAttribute("signUpForm", new SignUpForm());
+        return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm,
+                               Errors errors, Model model){
+        if (errors.hasErrors()){
+            return "account/sign-up";
+        }
+        accountService.processNewAccount(signUpForm);
+
+        return "redirect:/";
+    }
+}
