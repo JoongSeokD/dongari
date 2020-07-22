d5b45034e5a87844468cd1855d73aa3379a637eb
JoongSeokD <ljseokd@gmail.com>
2020-07-22 11:26:44 +0900

    이메일 인증 확인 추가

--- src/main/java/com/ljseokd/dongari/modules/account/AccountController.java
+++ src/main/java/com/ljseokd/dongari/modules/account/AccountController.java
@@ -1,5 +1,10 @@
 package com.ljseokd.dongari.modules.account;
 
+import com.ljseokd.dongari.modules.account.form.EmailForm;
+import com.ljseokd.dongari.modules.account.form.SignUpForm;
+import com.ljseokd.dongari.modules.account.security.CurrentAccount;
+import com.ljseokd.dongari.modules.account.validator.EmailValidator;
+import com.ljseokd.dongari.modules.account.validator.SignUpFormValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +22,18 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final AccountService accountService;
+    private final AccountRepository accountRepository;
     private final SignUpFormValidator signUpFormValidator;
+    private final EmailValidator emailValidator;
 
     @InitBinder("signUpForm")
-    public void initBinder(WebDataBinder webDataBinder){
+    public void signUpInitBinder(WebDataBinder webDataBinder){
         webDataBinder.addValidators(signUpFormValidator);
+    }
+
+    @InitBinder("emailForm")
+    public void emailInitBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(emailValidator);
     }
 
     @GetMapping("/sign-up")
@@ -39,5 +51,29 @@ public class AccountController {
         accountService.processNewAccount(signUpForm);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/check-email")
+    public String checkEmail(@CurrentAccount Account account, Model model){
+        model.addAttribute("email", account.getEmail());
+        return "account/check-email";
+    }
+
+    @GetMapping("/check-email-token")
+    public String checkEmailToken(@Valid EmailForm emailForm,
+            Errors errors, Model model){
+
+        String view = "account/checked-email";
+
+        if (errors.hasErrors()){
+            return view;
+        }
+
+        Account account = accountRepository.findByEmail(emailForm.getEmail());
+        accountService.completeSignUp(account);
+        model.addAttribute("numberOfUser", accountRepository.count());
+        model.addAttribute("nickname", account.getNickname());
+
+        return view;
     }
 }
