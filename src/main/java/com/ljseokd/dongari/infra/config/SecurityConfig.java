09930b8249a0a9aab0cac7831a7ca23662c3c3f9
JoongSeokD <ljseokd@gmail.com>
2020-07-22 11:29:19 +0900

    permitAll() /check-email-token 추가

--- src/main/java/com/ljseokd/dongari/infra/config/SecurityConfig.java
+++ src/main/java/com/ljseokd/dongari/infra/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ljseokd.dongari.infra.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .mvcMatchers("/", "/login", "/sign-up").permitAll()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token").permitAll()
                 .anyRequest().authenticated();
 
         http.formLogin()
